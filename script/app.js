da4becc43244d26e8ad48158735e76b0a37d3e0a
AnhhTuann <tuannguyen25101999@gmail.com>
2024-09-23 21:56:33 +0700

    add customer

---
+++ script/app.js
@@ -0,0 +1,10 @@
+$(".customer-list").slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  dots: true,
+  prevArrow:
+    "<button type='button' class='slick-prev pull-left'><i class='fa-solid fa-arrow-left' aria-hidden='true'></i></button>",
+  nextArrow:
+    "<button type='button' class='slick-next pull-right'><i class='fa-solid fa-arrow-right' aria-hidden='true'></i></button>",
+});
